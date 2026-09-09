5a0848ac52e420c5eb222ae6343a5e62187ff6ae
sarahi <le23080267@merida.tecnm.mx>
2026-09-08 18:27:42 -0600

    adding HelloWorld to test spring Configuration

---
+++ src/main/java/com/tecnm/merida/marketbackendv2263/MarketBackendV2263Application.java
@@ -0,0 +1,13 @@
+package com.tecnm.merida.marketbackendv2263;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketBackendV2263Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketBackendV2263Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/tecnm/merida/marketbackendv2263/MarketBackendV2263ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnm.merida.marketbackendv2263;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketBackendV2263ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
